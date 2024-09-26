29093896efa402ceec0b317496ab755e125b8aab
Roronoazoro-tss <mugiwara.zoro.tss@gmail.com>
2024-09-26 14:47:39 +0530

    config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,17 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6584789596"
-    sudo_users = ["5630057244", "2010819209", "5702598840", "6101457748", "6154972031", "1735664760", "7036005233", "6100011620", "7297953309", "6412447141", "7244871367", "5530116994", "6584789596", "949302414"]
-    GROUP_ID = "-1002000314620"
+    OWNER_ID = "6861906489"
+    sudo_users = ["6861906489"]
+    GROUP_ID = "-1002337505439"
     TOKEN = "6600186454:AAFLZH3zYUM3V1q_a7RkQ6nnGlHiz5hRxoU"
     mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg", "https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/32556c77847dff110577c.jpg", "https://telegra.ph//file/0650844fc5db4049959bc.jpg"]
-    SUPPORT_CHAT = "Grabbing_Your_WH_Group"
+    PHOTO_URL = ["https://envs.sh/SqR.jpg"]
     UPDATE_CHAT = "FLEX_BOTS_NEWS"
     BOT_USERNAME = "Grabbing_Your_Waifu_Bot"
     CHARA_CHANNEL_ID = "-1002009998662"
-    api_id = "20533795"
+    api_id = "24331006"
     api_hash = "f6cadf28523943f525e706e6ace8a250"
 
     
