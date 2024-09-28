0ddb4f9badd0a02f4e40efc5f60be4740d993e55
Roronoazoro-tss <mugiwara.zoro.tss@gmail.com>
2024-09-29 00:48:45 +0530

    config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = ["6861906489"]
     GROUP_ID = "-1002337505439"
     TOKEN = "7624228426:AAEC_qtuQoQh1KDr7xhl6uDZqk3f_2jHMk4"
-    mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://mybotwaifu123:<db_mybotwaifu123>@cluster0.48wu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     BOT_USERNAME = "make_your_waifu_bot"   
     api_id = "24331006"
     api_hash = "783a6c919b56b9dca14a16bc835fea26"
